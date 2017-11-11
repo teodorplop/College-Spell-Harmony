b939b72aa555bba123eee0e4e70d6f3a010dcca0
Teodor Plop <teodorplop@yahoo.com>
2017-11-11 22:08:57 +0200

    Changed some folders, less vertices

---
+++ Assets/Scripts/AnimationHandlerScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationHandlerScript : MonoBehaviour {
+	
+	private Animator DoorAnimator = null;
+	private bool ButtonPressed = false;
+    private bool isOpened = false;
+
+    void Start () 
+	{
+        DoorAnimator = GetComponent<Animator>();
+		DoorAnimator.SetBool("isOpen", false);
+		ButtonPressed = false;
+	}
+	
+    public void Triggered()
+    {
+        DoorAnimator.SetBool("isOpen", isOpened = !isOpened);
+    }
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			ButtonPressed = true;
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider collider)
+    {
+        ButtonPressed = false;
+				
+    }
+	
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (ButtonPressed && !DoorAnimator.GetBool("isOpen"))
+		{
+			DoorAnimator.SetBool("isOpen", true);
+			ButtonPressed = false;
+        }
+		
+		if (ButtonPressed && DoorAnimator.GetBool("isOpen"))
+		{
+			DoorAnimator.SetBool("isOpen", false);
+			ButtonPressed = false;
+        }
+		
+				
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+		//closing automatically the door 
+		//DoorAnimator.SetBool("isOpen", false);
+		//ButtonPressed = false;
+    }
+}﻿
+
